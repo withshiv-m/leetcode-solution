7ffda1a642c5a4f6442b98db9376574f0cba4ec3
shiv-m <mugleshiv1531@gmail.com>
2026-09-25 18:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (65.03%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        a = len(str1)
+        b = len(str2)
+        
+        while b != 0:
+            a, b = b, a % b
+        
+        return str1[:a]
